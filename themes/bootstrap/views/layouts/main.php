15c969042e8cc01ca9f2d687119ba052730cac42
Thinking Song <123wuming_123@163.com>
2013-08-08 02:08:10 +0800

    前段显示错误信息

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -47,6 +47,18 @@
 		)); ?><!-- breadcrumbs -->
 	<?php endif?>
 
+    <?php
+        $this->widget('bootstrap.widgets.TbAlert', array(
+            'block'=>true, // display a larger alert block?
+            'fade'=>true, // use transitions?
+            'closeText'=>'&times;', // close link text - if set to false, no close link is displayed
+            'alerts'=>array( // configurations per alert type
+                'success'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), 
+                'error'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), 
+            ),
+        ));
+    ?>
+
     <div class="row">
         <div class="span2">
         </div>
