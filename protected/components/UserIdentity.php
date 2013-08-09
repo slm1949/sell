56553b46430fa25d844ad03951adb23ce7619fb7
Thinking Song <123wuming_123@163.com>
2013-08-09 23:10:29 +0800

    添加登陆密码验证

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -18,13 +18,11 @@ class UserIdentity extends CUserIdentity
 	public function authenticate()
 	{
 		$users=array(
-			// username => password
-			'demo'=>'demo',
 			'admin'=>'admin',
 		);
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
+		elseif(Yii::app()->settings->get('system', 'admin_password')!==$this->password)
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
 		else
 			$this->errorCode=self::ERROR_NONE;
