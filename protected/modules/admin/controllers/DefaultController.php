7e2d163a51cedbc6353a7a465d911d17a03c3e69
Thinking Song <123wuming_123@163.com>
2013-08-13 15:33:54 +0800

    给后台默认首页和配置加上权限验证

--- protected/modules/admin/controllers/DefaultController.php
+++ protected/modules/admin/controllers/DefaultController.php
@@ -2,6 +2,35 @@
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }   
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow',
+                'actions'=>array('index'),
+                'users'=>array('admin'),
+            ),
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+
 	public function actionIndex()
 	{
 		$this->render('index');
